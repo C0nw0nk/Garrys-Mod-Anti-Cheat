6cf81de05d86051a1eeae239d68791b6fb6155a3
C0nw0nk <C0nw0nks@googlemail.com>
2016-07-14 16:17:07 +0100

    Update anti-recoil.lua
    Fix issue if SWEP has no recoil method attached so now only weapons that are suppose to recoil will recoil when fired.

--- garrysmod/lua/autorun/server/anti-recoil.lua
+++ garrysmod/lua/autorun/server/anti-recoil.lua
@@ -17,19 +17,19 @@ hook.Add("StartCommand", "StartCommand-Anti-Recoil", function(pl, key)
 	if pl:KeyDown(IN_ATTACK) and pl:GetActiveWeapon():Clip1() != nil and pl:GetActiveWeapon():Clip1() > 0 then
 		--If the weapon they are holding has a recoil method attached to it by the SWEP.
 		--If the recoil method is kickup.
-		if pl:GetActiveWeapon().Primary.KickUp then
+		if pl:GetActiveWeapon().Primary.KickUp != nil then
 			--Force the player to look up while primary firing.
 			pl:SetEyeAngles( pl:EyeAngles() +  Angle(-pl:GetActiveWeapon().Primary.KickUp,0,0) ) --Angle (Up,Down | Left,Right | Screen rotate)
 		end
 		
 		--If the recoil method is kickdown.
-		if pl:GetActiveWeapon().Primary.KickDown then
+		if pl:GetActiveWeapon().Primary.KickDown != nil then
 			--Force the player to look down while primary firing.
 			pl:SetEyeAngles( pl:EyeAngles() +  Angle(pl:GetActiveWeapon().Primary.KickDown,0,0) ) --Angle (Up,Down | Left,Right | Screen rotate)
 		end
 		
 		--If the recoil method is recoil.
-		if pl:GetActiveWeapon().Primary.Recoil then
+		if pl:GetActiveWeapon().Primary.Recoil != nil then
 			--Force the player to look down while primary firing.
 			pl:SetEyeAngles( pl:EyeAngles() +  Angle(pl:GetActiveWeapon().Primary.Recoil,0,0) ) --Angle (Up,Down | Left,Right | Screen rotate)
 		end
@@ -39,19 +39,19 @@ hook.Add("StartCommand", "StartCommand-Anti-Recoil", function(pl, key)
 	if pl:KeyDown(IN_ATTACK2) and pl:GetActiveWeapon():Clip1() != nil and pl:GetActiveWeapon():Clip1() > 0 then
 		--If the weapon they are holding has a recoil method attached to it by the SWEP.
 		--If the recoil method is kickup.
-		if pl:GetActiveWeapon().Secondary.KickUp then
+		if pl:GetActiveWeapon().Secondary.KickUp != nil then
 			--Force the player to look up secondary firing.
 			pl:SetEyeAngles( pl:EyeAngles() +  Angle(-pl:GetActiveWeapon().Secondary.KickUp,0,0) ) --Angle (Up,Down | Left,Right | Screen rotate)
 		end
 		
 		--If the recoil method is kickdown.
-		if pl:GetActiveWeapon().Secondary.KickDown then
+		if pl:GetActiveWeapon().Secondary.KickDown !=nil then
 			--Force the player to look down while secondary firing.
 			pl:SetEyeAngles( pl:EyeAngles() +  Angle(pl:GetActiveWeapon().Secondary.KickDown,0,0) ) --Angle (Up,Down | Left,Right | Screen rotate)
 		end
 		
 		--If the recoil method is recoil.
-		if pl:GetActiveWeapon().Secondary.Recoil then
+		if pl:GetActiveWeapon().Secondary.Recoil != nil then
 			--Force the player to look down while secondary firing.
 			pl:SetEyeAngles( pl:EyeAngles() +  Angle(pl:GetActiveWeapon().Secondary.Recoil,0,0) ) --Angle (Up,Down | Left,Right | Screen rotate)
 		end
