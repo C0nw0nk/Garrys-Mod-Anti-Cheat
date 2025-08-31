cfe9f6d12f1b64cd502f0480541d9a4a4b7708fe
Conor McKnight <12204587+C0nw0nk@users.noreply.github.com>
2025-08-31 16:29:21 +0100

    Delete garrysmod/lua/autorun directory

--- garrysmod/lua/autorun/send-lua-and-net-send.lua
+++
@@ -1,7 +0,0 @@
---Everything below this point is not necessary if you block sv_allowcslua people only change these
---values if they can use sv_allowcslua to do so.(Force changing your client convars)
-
-if SERVER then
-end
-if CLIENT then
-end
